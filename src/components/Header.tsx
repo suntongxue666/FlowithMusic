adc871ceae95ea4c23a44c38c2cf04ab6b8d0054
apple <1234>
2026-04-05 15:53:51 +0800

    调整会员皇冠位置2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -473,7 +473,7 @@ export default function Header({ currentPage }: HeaderProps) {
 
           .premium-crown-badge {
             position: absolute;
-            top: -36px;
+            top: -32px;
             right: -8px;
             font-size: 14px;
             z-index: 5;
